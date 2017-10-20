ffe3f2869b186c9c894bda007dc1eddb5f751c0d
Manuele Ferreira <manueleferreira@gmail.com>
2017-10-19 23:00:04 -0200

    #3 improve some creation unit tests

---
+++ src/test/java/br/com/xyinc/repository/BusinessDomainInstanceRepositoryTest.java
@@ -0,0 +1,65 @@
+package br.com.xyinc.repository;
+
+import br.com.xyinc.model.BusinessDomain;
+import br.com.xyinc.model.BusinessDomainInstance;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by manuele on 19/10/17.
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class BusinessDomainInstanceRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private BusinessDomainInstanceRepository businessDomainInstanceRepository;
+
+    @Test
+    public void findByIdThenReturnBusinessDomainInstance() {
+        BusinessDomain businessDomain = new BusinessDomain();
+        businessDomain.setName("pencil");
+        entityManager.persist(businessDomain);
+        entityManager.flush();
+
+        BusinessDomainInstance instance = new BusinessDomainInstance();
+        instance.setBusinessDomain(businessDomain);
+        entityManager.persist(instance);
+        entityManager.flush();
+
+        BusinessDomainInstance found = businessDomainInstanceRepository.
+                findById(instance.getId());
+
+        assertThat(found.getId()).isEqualTo(instance.getId());
+    }
+
+    @Test
+    public void findByBusinessDomainThenReturnBusinessDomainInstance() {
+        BusinessDomain businessDomain = new BusinessDomain();
+        businessDomain.setName("pencil");
+        entityManager.persist(businessDomain);
+        entityManager.flush();
+
+        BusinessDomainInstance instance = new BusinessDomainInstance();
+        instance.setBusinessDomain(businessDomain);
+        entityManager.persist(instance);
+        entityManager.flush();
+
+        List<BusinessDomainInstance> foundList = businessDomainInstanceRepository.
+                findByBusinessDomain(instance.getBusinessDomain());
+
+        assertThat(foundList.size()).isEqualTo(1);
+        assertThat(foundList.get(0).getId()).isEqualTo(instance.getId());
+    }
+}
